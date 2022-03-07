b9a5fb701909eb75a08178a65d31a27ca4014492
k-rea <kitahara@k-rea.com>
2022-03-07 11:16:20 +0900

    Change for  next.js

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 locals {
-  profile       = "my_profile"
+  profile       = "default"
   region        = "ap-northeast-1"
-  app_name      = "app_name"
-  domain        = "my_domain"
-  domain_prefix = "app-name"
+  app_name      = "tf-nextjs-amplify"
+  domain        = "testdomain.com"
+  domain_prefix = "www"
 }
 
 provider "aws" {
@@ -11,10 +11,6 @@ provider "aws" {
   profile = local.profile
 }
 
-resource "aws_codecommit_repository" "static_site_amplify" {
-  repository_name = local.app_name
-  description     = "This is static site by amplify"
-}
 
 data "aws_iam_policy_document" "assume_role" {
   statement {
@@ -26,18 +22,34 @@ data "aws_iam_policy_document" "assume_role" {
     }
   }
 }
-resource "aws_iam_role" "amplify-codecommit" {
-  name                = "${local.app_name}_AmplifyCodeCommit"
+resource "aws_iam_role" "amplify_role" {
+  name                = "amplify_deploy_terraform_role"
   assume_role_policy  = join("", data.aws_iam_policy_document.assume_role.*.json)
   managed_policy_arns = ["arn:aws:iam::aws:policy/AWSCodeCommitReadOnly"]
 }
 
+resource "aws_iam_role_policy" "amplify_role_policy" {
+  name = "amplify_iam_role_policy"
+  role = aws_iam_role.amplify_role.id
+
+  policy = file("${path.cwd}/iam/amplify_role_policies.json")
+}
+
 resource "aws_amplify_app" "static_site_amplify" {
   name                     = local.app_name
-  repository               = aws_codecommit_repository.static_site_amplify.clone_url_http
-  iam_service_role_arn     = aws_iam_role.amplify-codecommit.arn
-  enable_branch_auto_build = true
+  repository               = "https://git-codecommit.ap-northeast-1.amazonaws.com/v1/repos/test-nextjs-amplify"
+  iam_service_role_arn     = aws_iam_role.amplify_role.arn
   build_spec               = file("./build_spec.yml")
+  enable_branch_auto_build = true
+  enable_auto_branch_creation = true
+  enable_branch_auto_deletion = true
+  platform = "WEB"
+  # Comment this on the first run, trigger a build of your branch, This will added automatically on the console after deployment. Add it here to ensure your subsequent terraform runs don't break your amplify deployment.
+#  custom_rule {
+#    source = "/<*>"
+#    target = "https://xxx.cloudfront.net/<*>"
+#    status = "200"
+#  }
   custom_rule {
     source = "/<*>"
     target = "/index.html"
@@ -45,6 +57,9 @@ resource "aws_amplify_app" "static_site_amplify" {
   }
   environment_variables    = {
     ENV = "dev"
+    _LIVE_UPDATES = <<ENV
+[{"pkg":"next-version","type":"internal","version":"latest"}]
+ENV
   }
 }
 
@@ -52,6 +67,8 @@ resource "aws_amplify_branch" "main" {
   app_id      = aws_amplify_app.static_site_amplify.id
   branch_name = "main"
   stage       = "PRODUCTION"
+  framework = "Next.js - SSR"
+  enable_auto_build = true
 }
 
 resource "aws_amplify_domain_association" "static_site_amplify" {
